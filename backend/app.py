ed5bc5548c82373be077b61773cb521a99d82d4d
RenatoResi <renatomeirelles@gmail.com>
2025-09-05 15:30:44 -0300

    ajuste

--- backend/app.py
+++ backend/app.py
@@ -1280,4 +1280,4 @@ def atualizar_especie(id):
         session.close()
 
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=True, host='0.0.0.0', port=5001)
